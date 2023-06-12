dd19f2f5633f1b1e0e56d73a7672b6eb6c2ecbd1
sdxtcqs <870663413@qq.com>
2023-06-12 18:33:40 +0800

    First Commit

---
+++ src/main/java/nju/websoft/chinaopendataportal/ChinaOpenDataPortalApplication.java
@@ -0,0 +1,13 @@
+package nju.websoft.chinaopendataportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaOpenDataPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinaOpenDataPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nju/websoft/chinaopendataportal/ChinaOpenDataPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package nju.websoft.chinaopendataportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinaOpenDataPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
